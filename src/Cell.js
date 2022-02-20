3b6a3c14362cc909bdc3765bba032d1a09b6aeb9
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-02-19 21:47:41 -0800

    commit tree 87dc1ddc7e8190de885d3dec4451e811d4748c6b

--- src/Cell.js
+++ src/Cell.js
@@ -3,17 +3,34 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
   
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      color: this.props.color
-    }
+      color: this.props.color,
+    };
   }
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.selectedColor,
+    });
+  };
+
+  makeColorSwatches = () => (
+    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
+      let callback = () => this.props.setSelectedColor(str)
+      return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+    })
+  )
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
-      </div>
-    )
+      <div
+        onClick={callback}
+        key={idx}
+        className="color-swatch"
+        style={{ backgroundColor: str }}
+      />
+    );
   }
   
 }
